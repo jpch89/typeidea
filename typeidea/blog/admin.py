c9dee8e0ac4ab11277a95e31198e08dfb2fc1540
进击的团子 <jpch89@outlook.com>
2019-07-10 20:33:12 +0800

    add admin for Post, edit link disabled

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from .models import Post, Category, Tag
 
@@ -17,6 +19,45 @@ class CategoryAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator',
+    ]
+    # 这样并不能禁用编辑
+    # list_display_links = []
+    # 这样才行
+    list_display_links = None
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    acitons_on_bottom = True
+
+    # 编辑页面
+    save_on_top = True
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('admin:blog_post_change', args=(obj.id, ))
+        )
+    operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user

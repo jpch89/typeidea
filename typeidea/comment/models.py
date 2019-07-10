8dbb42c03e5c684db73ede55e6e34fee0b10e9e2
进击的团子 <jpch89@outlook.com>
2019-07-10 22:08:53 +0800

    fix bug in model Comment

--- typeidea/comment/models.py
+++ typeidea/comment/models.py
@@ -13,6 +13,7 @@ class Comment(models.Model):
     target = models.ForeignKey(Post, verbose_name='评论目标')
     content = models.CharField(max_length=2000, verbose_name='内容')
     nickname = models.CharField(max_length=50, verbose_name='昵称')
+    website = models.URLField(verbose_name='网站')
     email = models.EmailField(verbose_name='邮箱')
     status = models.PositiveIntegerField(
         default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name='状态')

9425760faa5f2a99845003880bdd8af0eaf58910
进击的团子 <jpch89@outlook.com>
2019-07-10 20:11:47 +0800

    modify admin for Category and Tag

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -8,8 +8,16 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time')
     fields = ('name', 'status', 'is_nav')
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)
 
-@admin.register(admin.ModelAdmin)
+
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super().save_model(request, obj, form, change)

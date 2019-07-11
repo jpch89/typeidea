56f7cbffbe7d93e72ee970745de6ba84d16315bf
团子大圆帅 <jpch89@outlook.com>
2019-07-11 21:32:21 +0800

    modify blog/views.py

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,6 +1,8 @@
 # from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Post, Category, Tag
+
 
 def post_list(request, category_id=None, tag_id=None):
     # content = 'post_list category_id={category_id}, tag_id={tag_id}'.format(
@@ -9,9 +11,30 @@ def post_list(request, category_id=None, tag_id=None):
     # )
 
     # return HttpResponse(content)
-    return render(request, 'blog/list.html', context={'name': 'post_list'})
+
+    if tag_id:
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            post_list = []
+        else:
+            post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+    else:
+        post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+        if category_id:
+            post_list = post_list.filter(category_id=category_id)
+
+    # return render(request, 'blog/list.html', context={'name': 'post_list'})
+    return render(request, 'blog/list.html', context={'post_list': post_list})
 
 
-def post_detail(request, post_id):
+def post_detail(request, post_id=None):
     # return HttpResponse('detail')
-    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+
+    try:
+        post = Post.objects.get(id=post_id)
+    except Post.DoesNotExist:
+        post = None
+
+    # return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+    return render(request, 'blog/detail.html', context={'post': post})

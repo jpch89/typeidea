d80dcf8aa4195caf20571fdbcb588bfd6f8c6d4c
团子大圆帅 <jpch89@outlook.com>
2019-07-14 00:51:19 +0800

    add IndexView and CommonViewMixin

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -5,27 +5,6 @@ from .models import Post, Category, Tag
 from config.models import SideBar
 
 
-# def post_list(request, category_id=None, tag_id=None):
-#     tag = None
-#     category = None
-
-#     if tag_id:
-#         post_list, tag = Post.get_by_tag(tag_id)
-#     elif category_id:
-#         post_list, category = Post.get_by_category(category_id)
-#     else:
-#         post_list = Post.latest_posts()
-
-#     context = {
-#         'category': category,
-#         'tag': tag,
-#         'post_list': post_list,
-#         'sidebars': SideBar.get_all(),
-#     }
-#     context.update(Category.get_navs())
-#     return render(request, 'blog/list.html', context=context)
-
-
 class PostListView(ListView):
     queryset = Post.latest_posts()
     paginate_by = 1
@@ -34,21 +13,21 @@ class PostListView(ListView):
     template_name = 'blog/list.html'
 
 
-# def post_detail(request, post_id=None):
-#     try:
-#         post = Post.objects.get(id=post_id)
-#     except Post.DoesNotExist:
-#         post = None
-
-#     context = {
-#         'post': post,
-#         'sidebars': SideBar.get_all(),
-#     }
-#     context.update(Category.get_navs())
-
-#     return render(request, 'blog/detail.html', context=context)
-
-
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
+
+
+class CommonViewMixin:
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'sidebars': SideBar.get_all()})
+        context.update(Category.get_navs())
+        return context
+
+
+class IndexView(CommonViewMixin, ListView):
+    queryset = Post.lates_posts()
+    paginate_by = 5
+    context_object_name = 'post_list'
+    template_name = 'blog/list.html'

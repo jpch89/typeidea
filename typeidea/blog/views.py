ad87497d45d74e5e6a9b03af3fe6ff2677d5f16d
团子大圆帅 <jpch89@outlook.com>
2019-07-14 09:11:10 +0800

    modify urls.py and add Views

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,21 +1,9 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
 from .models import Post, Category, Tag
 from config.models import SideBar
-
-
-class PostListView(ListView):
-    queryset = Post.latest_posts()
-    paginate_by = 1
-    # 如果不设置此项，在模板中需要使用 object_list 变量
-    context_object_name = 'post_list'
-    template_name = 'blog/list.html'
-
-
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'blog/detail.html'
 
 
 class CommonViewMixin:
@@ -29,5 +17,45 @@ class CommonViewMixin:
 class IndexView(CommonViewMixin, ListView):
     queryset = Post.latest_posts()
     paginate_by = 5
+    # 如果不设置此项，在模板中需要使用 object_list 变量
     context_object_name = 'post_list'
     template_name = 'blog/list.html'
+
+
+class CategoryView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_id = self.kwargs.get('category_id')
+        category = get_object_or_404(Category, pk=category_id)
+        context.update({'category': category})
+        return context
+
+    def get_queryset(self):
+        """重写 queryset，根据分类过滤"""
+        queryset = super().get_queryset()
+        category_id = self.kwargs.get('category_id')
+        return queryset.filter(category_id=category_id)
+
+
+class TagView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        tag_id = self.kwargs.get('tag_id')
+        tag = get_object_or_404(Tag, pk=tag_id)
+        context.update({'tag': tag})
+        return context
+
+    def get_queryset(self):
+        """重写 queryset，根据标签过滤"""
+        queryset = super().get_queryset()
+        tag_id = self.kwargs.get('tag_id')
+        return queryset.filter(tag_id=tag_id)
+
+
+class PostDetailView(CommonViewMixin, DetailView):
+    queryset = Post.latest_posts()
+    template_name = 'blog/detail.html'
+    # 在模板中需要使用的上下文名字
+    context_object_name = 'post'
+    # 用于接收来自 url 的主键，根据这个主键进行查询
+    pk_url_kwarg = 'post_id'
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -1,32 +1,20 @@
-"""typeidea URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
 from django.contrib import admin
 
-from blog.views import PostListView, PostDetailView
+from blog.views import (
+    IndexView, CategoryView, TagView,
+    PostDetailView
+)
 from config.views import links
 from typeidea.custom_site import custom_site
 
 urlpatterns = [
-    url(r'^$', PostListView.as_view(), name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
     url(r'^category/(?P<category_id>\d+)/$',
-        PostListView.as_view(),
+        CategoryView.as_view(),
         name='category-list'),
-    url(r'^tag/(?P<tag_id>\d+)/$', PostListView.as_view(), name='tag-list'),
-    url(r'^post/(?P<pk>\d+).html$',
+    url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    url(r'^post/(?P<post_id>\d+).html$',
         PostDetailView.as_view(),
         name='post-detail'),
     url(r'^links/$', links, name='links'),

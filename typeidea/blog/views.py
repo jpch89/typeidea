11ad8c5d4a45d9cc9292c5b643e1e6ff54f3dd14
团子大圆帅 <jpch89@outlook.com>
2019-07-15 22:11:29 +0800

    middleware to count pv, uv

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,6 +1,7 @@
-# from comment.forms import CommentForm
-# from comment.models import Comment
-from django.db.models import Q
+from datetime import date
+
+from django.core.cache import cache
+from django.db.models import Q, F
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
@@ -65,15 +66,32 @@ class PostDetailView(CommonViewMixin, DetailView):
     # 用于接收来自 url 的主键，根据这个主键进行查询
     pk_url_kwarg = 'post_id'
 
-    # 用自定义模板标签实现了
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context.update({
-    #         'comment_form': CommentForm,
-    #         # self.request.path 是不包括参数的当前 url，也不包含端口号及前面的内容
-    #         'comment_list': Comment.get_by_target(self.request.path),
-    #     })
-    #     return context
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        self.handle_visited()
+        return response
+
+    def handle_visited(self):
+        increase_pv = False
+        increase_uv = False
+        uid = self.request.uid
+        pv_key = 'pv:%s:%s' % (uid, self.request.path)
+        uv_key = 'uv:%s:%s:%s' % (uid, str(date.today()), self.request.path)
+
+        if not cache.get(pv_key):
+            increase_pv = True
+            cache.set(pv_key, 1, 1 * 60)
+
+        if not cache.get(uv_key):
+            increase_uv = True
+            cache.set(uv_key, 1, 24 * 60 * 60)
+
+        if increase_pv and increase_uv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1, uv=F('uv') + 1)
+        elif increase_pv:
+            Post.objects.filter(pk=self.object.id).update(pv=F('pv') + 1)
+        elif increase_uv:
+            Post.objects.filter(pk=self.object.id).update(uv=F('uv') + 1)
 
 
 class SearchView(IndexView):

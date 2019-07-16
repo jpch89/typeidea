820fd5e10417319ccbf875f61c0b27e93fbcf355
团子大圆帅 <jpch89@outlook.com>
2019-07-16 11:01:16 +0800

    modify base_admin

--- typeidea/typeidea/base_admin.py
+++ typeidea/typeidea/base_admin.py
@@ -10,10 +10,19 @@ class BaseOwnerAdmin(object):
     # 对应于 fields，是不想显示的字段
     exclude = ('owner', )
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
+    def get_list_queryset(self):
+        request = self.request
+        qs = super().get_list_queryset()
         return qs.filter(owner=request.user)
 
-    def save_model(self, request, obj, form, change):
-        obj.owner = request.user
-        return super().save_model(request, obj, form, change)
+    # def get_queryset(self, request):
+    #     qs = super().get_queryset(request)
+    #     return qs.filter(owner=request.user)
+
+    def save_models(self):
+        self.new_obj.owner = self.request.user
+        return super().save_models()
+
+    # def save_model(self, request, obj, form, change):
+    #     obj.owner = request.user
+    #     return super().save_model(request, obj, form, change)
